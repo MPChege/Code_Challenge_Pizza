ebde0e4a216f6915403366942ffe18f82d73d1fc
MPChege <mark.wanyoike@strathmore.edu>
2023-07-10 13:25:52 +0300

    firsy commit

---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Rails.application.routes.draw do
+  resources :resturant_pizzas
+  resources :pizzas
+  resources :resturants
+  end
---
+++ db/seeds.rb
@@ -0,0 +1,31 @@
+puts "🌱 Seeding pizzas..."
+
+10.times do
+  Pizza.create(
+    pizza_name: Faker::Food.dish,
+    pizza_size: Faker::Food.metric_measurement,
+    ingredients: Faker::Food.ingredient,
+    
+  )
+end
+
+# Create 5 sets of fake data for the "resturant_pizzas" table
+5.times do
+  ResturantPizza.create(
+    price: Faker::Number.between(from: 1, to: 30),
+    resturant_id: Faker::Number.between(from: 1, to: 10),
+    pizza_id: Faker::Number.between(from: 1, to: 10)
+  )
+end
+
+# Create 10 sets of fake data for the "resturants" table
+10.times do
+  Resturant.create(
+    resturant_name: Faker::Restaurant.name,
+    address: Faker::Address.full_address,
+    created_at: Faker::Time.between(from: DateTime.now - 30, to: DateTime.now),
+    updated_at: Faker::Time.between(from: DateTime.now - 30, to: DateTime.now)
+  )
+end
+
+puts "✅ Done seeding!"
